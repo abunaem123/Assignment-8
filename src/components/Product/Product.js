1ebaa1d12bc2e96bd01a7eade56ce9c601bd7e79
abunaem123 <88497497+abunaem123@users.noreply.github.com>
2022-03-26 23:21:08 +0600

    Almost done

---
+++ src/components/Product/Product.js
@@ -0,0 +1,36 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+const Product = (props) => {
+    const { img, name, price, } = props.product;
+
+    return (
+        // <div className='product col'>
+        //     <img src={img} alt="" />
+        //     <div className='product-info'>
+        //         <h3 className='product-name'>{name}</h3>
+        //         <p>Price: {price}</p>
+        //         <p>Id: {id}</p>
+        //     </div>
+        //     <button onClick={() => props.handleAddToCart(props.product)} className='cart-btn'>
+        //         <p>Add to Cart</p>
+        //     </button>
+        // </div>
+        <div className="col">
+            <div className="card h-300 shadow-sm">
+                <img src={img} className="card-img-top w-50 mx-auto pt-2" alt="" />
+                <div className="card-body h-400">
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text pb-4">Price: {price}</p>
+                </div>
+                <button onClick={() => props.handleAddToCart(props.product)} className='cart-btn  p-2 d-flex align-items-center justify-content-center'>
+                    <p className='btn-text fw-normal pe-2'>Add to Cart</p>
+                    {/* <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon> */}
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    const handleAddToCart = (product) => {
+        console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart)
+    }
+    const deleteButton = () => {
+        const deleteItem = [];
+        setCart(deleteItem)
+    }
+    return (
+        <div className='shop-container'>
+            <div className="product-container row row-cols-1 row-cols-md-3 g-4 p-4">
+                {
+                    products.map(product => <Product key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container bg-light p-4">
+                <h4>Order Summary</h4>
+                {/* <p>Selected Items: {cart.length}</p> */}
+                {cart.map((item) =>
+                    <div key={item.id} className='d-flex align-items-center p-2'>
+                        <div className='m-2 p-2 border bg-light rounded-circle'>
+                            <img src={item.img} width="30px" height="30px" alt="" />
+                        </div>
+                        <h6>{item.name}</h6>
+                    </div>
+                    // <div>
+                    //     <img src={item.img} alt="" />
+                    //     <p key={item.id}>{item.name}</p>
+                    // </div>
+                )}
+                <div className='d-flex flex-column'>
+                    <button className='m-2 border border-3 border-dark'>Choose 1 for Me </button>
+                    <button onClick={deleteButton} className='m-2 border border-3 border-dark'>Choose Again </button>
+                </div>
+                {/* <button >Buy One</button>
+            <br />
+            <button onClick={deleteButton} className='mt-2'>Choose Again</button> */}
+            </div>
+        </div >
+    );
+};
+
+export default Shop;
